c4db35e5652cba69d482588c0f6d6b249edbdd66
Arjun <arjunsohur@gmail.com>
2023-08-16 17:24:27 -0400

    first commit

---
+++ newspigeon/prefs/views.py
@@ -0,0 +1,92 @@
+from django.shortcuts import render
+import json
+
+initialPreferences = [
+    [
+        ["Business", 5],
+        ["Finance", 5],
+        ["Economics", 5],
+        ["Startup", 5],
+    ],
+    [
+        ["Science", 5],
+        ["Computer Science", 5],      
+        ["Artificial Intelligence", 5],
+        ["Technology", 5],
+        ["Medical Research", 5],
+        ["Pharmaceutical", 5],
+    ],
+    [
+        ["Entertainment", 5],
+        ["Weather", 5],
+        ["Art", 5],
+        ["Health", 5],
+        ["Lifestyle", 5],
+        ["Celebrity", 5],
+        ["Fashion and Beauty", 5],
+        ["Travel", 5],
+    ],
+    [
+        ["Politics", 5],
+        ["US Politics", 5],
+        ["UK Politics", 5],
+        ["European Politics", 5],
+        ["Canadian Politics", 5],
+        ["South America", 5],
+        ["World", 5],
+        ["Middle East", 5],
+        ["Africa", 5],
+        ["China", 5],
+        ["Asia", 5],
+    ],
+    [
+        ["Sports", 5],
+        ["Soccer/Football", 5],
+        ["American Football", 5],
+        ["Basketball", 5],
+        ["Hockey", 5],
+        ["Baseball", 5],
+        ["Tennis", 5],
+        ["Rugby", 5],
+        ["Cricket", 5]
+    ]
+  ]
+
+initialCategoryRatings = [
+    ["Business, Finance, and Economics", 5],
+    ["Science and Technology", 5],
+    ["Entertainment, Art, and Health", 5],
+    ["Domestic and World Politics", 5],
+    ["Sports", 5]
+
+  ]
+
+initialUnusedTopics = []
+
+
+user_prefs = []
+
+for idx, category_data in enumerate(initialCategoryRatings):
+    category_name, category_rating = category_data
+    prefs_data = {
+        "category": category_name,
+        "prefs": initialPreferences[idx],
+        "category_rating": category_rating,
+        "unused_topics": initialUnusedTopics
+    }
+    user_prefs.append(prefs_data)
+
+
+
+def home(request):
+    prefs = {
+        "user_prefs": user_prefs
+
+    }
+
+    return render(request, "prefs/home.html", prefs)
+
+
+
+
+    
