43fefd6d69cf51fecd16d97eb95e51d8251138e2
Arjun <arjunsohur@gmail.com>
2023-08-17 13:38:00 -0400

    Added admin and created database

--- newspigeon/prefs/models.py
+++ newspigeon/prefs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserPrefs(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    preferences_json = models.TextField()  
+    category_ratings_json = models.TextField()  
+    unused_topics_json = models.TextField()  
+
