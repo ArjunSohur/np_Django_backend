d560d51e221856d158eefb6ec1feae1bbf1fc20c
Arjun <arjunsohur@gmail.com>
2023-08-29 20:53:28 -0400

    Start of Docker and scrpa ecron job

--- newspigeon/home/admin.py
+++ newspigeon/home/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import SubjectVector, NewsArticle, PickledUser
+from .models import SubjectVector, NewsArticle, PickledUser, ArticleRating
 
 admin.site.register(SubjectVector)
 admin.site.register(NewsArticle)
 admin.site.register(PickledUser)
+admin.site.register(ArticleRating)
+
 
--- newspigeon/newspigeon/settings.py
+++ newspigeon/newspigeon/settings.py
@@ -78,22 +78,15 @@ WSGI_APPLICATION = "newspigeon.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-
     'default': {
-
         'ENGINE': 'django.db.backends.postgresql',
-
         'NAME': 'newspigeon',
-
         'USER': 'postgres',
-
         'PASSWORD': 'NewsPigeon',
-
-        'HOST': 'localhost',
-
-        'PORT': '5433',
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
+
 
 
 # Password validation
