1c7343afd201dd95528821ab15515ddbfc185b82
Arjun <arjunsohur@gmail.com>
2023-09-11 17:41:57 -0400

    all I needed to do was go to localhost rather than localhost:8000.  I am going to go cry now

--- newspigeon/newspigeon/settings.py
+++ newspigeon/newspigeon/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-&sox9@-u*u8at+_vub(3-f6ssw)0np#urruteezda)yw9)l(lg"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", default=0))
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -128,10 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static'),
-]
-STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
