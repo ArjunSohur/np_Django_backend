fe703622ccb514bb10c21dd87b445f13485de6f7
Arjun <arjunsohur@gmail.com>
2023-09-22 15:16:09 -0500

    at school

--- newspigeon/newspigeon/urls.py
+++ newspigeon/newspigeon/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
 from django.urls import path, include
 from users import views as user_views
+from . import settings
 
 
 urlpatterns = [
